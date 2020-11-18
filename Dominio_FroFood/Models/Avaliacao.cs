f92531277807e58724a11f5ccfb0a4e933639586
Cris Andrade <cristina.andrade@al.infnet.edu.br>
2020-11-18 18:45:53 -0300

    Add project files.

---
+++ Dominio_FroFood/Models/Avaliacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio_FroFood.Models
+{
+    public class Avaliacao
+    {
+        public Guid Id { get; set; }
+        public string Comentario { get; set; }
+        public int Nota { get; set; }
+    }
+}
